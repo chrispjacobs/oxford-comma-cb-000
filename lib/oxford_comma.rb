2c7b1aeeafe643652d7facecb142d7dcd48d89ed
Christopher Jacobs <chrisj.3222@gmail.com>
2019-02-22 23:22:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 def oxford_comma(array)
-  array[-1] = "and " + array.last
-  array.join(", ")
+  if array.count = 1
+    array.join
+  elsif array.count = 2
+    array.join(" and ")
+  else
+    array[-1] = "and " + array.last
+    array.join(", ")
+  end
 end
 
 array_example = ["Harry", "Ron", "Hermoine"]
